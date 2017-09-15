37b3a533955665d79f0cc3937cac83125f283ab5
Jeannie <jflaherty@utexas.edu>
2017-09-15 11:00:10 -0500

    created arrays for waiting and table

---
+++ data/table-data.js
@@ -0,0 +1,8 @@
+var tableArray = {
+  [customerName: "Mickey",
+  customerEmail:"themouse@theclub.com",
+  customerID: "themouseinthehouse",
+  phoneNumber: "222100cool"
+]
+};
+module.exports= tableArray;
---
+++ data/waiting-data.js
@@ -0,0 +1,8 @@
+var tableArray = {
+  [customerName: "Minnie",
+  customerEmail:"missmouse@theclub.com",
+  customerID: "girlmouseinthehouse",
+  phoneNumber: "222t00cool"
+]
+};
+module.exports= waitingArray;
